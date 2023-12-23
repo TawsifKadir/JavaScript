26017a2d569a2a443c38ecadeded0a377208722f
TawsifKadir <tawsif.kadir18@gmail.com>
2023-12-24 03:59:52 +0600

    My commit example

--- first.js
+++ first.js
@@ -1,1 +1,8 @@
-alert("Hello world");
+let gameNum = 25;
+let userNum = prompt("Please enter a number");
+
+while(userNum != gameNum){
+    userNum = prompt("Wrong! Please enter another number");
+}
+
+console.log("Congratulations! You have won the game");
